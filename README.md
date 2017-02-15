149bfbc1b6746477372082b8dce21444c7c9e9d3
abhishekk <abhishekk@PRINHYLTPAP0916.local>
2017-02-15 18:07:00 +0530

    update page and json

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# ई-बुक
-readme
+# Apna Pustakalaya (अपना पुस्तकालय)
+ ebook  (ई-बुक)
+
+apnapustakalaya ia a collection of ebook.
+
+Read Books, discover new fields, and muscle your brain for free !
+
+check it out : http://abhishek-jaiswal.github.io/apna-pustakalaya/
+
+
+## About us
+
+The revolunet team is specialised in rich web applications development, for mobile or desktop. 
+
+Check out our website : http://abhishek-jaiswal.github.io/
+
---
+++ update.py
@@ -0,0 +1,48 @@
+import os
+import json
+import requests
+from pdfminer.pdfparser import PDFParser
+from pdfminer.pdfdocument import PDFDocument
+from wand.image import Image
+
+
+books = []  
+
+s_dir = 'assets/books/'
+s_cover_dir = 'assets/covers/'
+s_img_ext = '.gif'
+
+def convert_image(pdf_file, image_name):
+	"""
+	"""
+   	with Image(filename=pdf_file+"[0]") as img:
+		img.save(filename=image_name)
+
+
+for f in os.listdir(s_dir):
+	if f.endswith('.pdf'):
+		pdf_file = s_dir + f
+		parser = PDFParser(open(pdf_file,  'rb'))
+		doc_info = PDFDocument(parser).info[0]
+		# create cover
+		title = doc_info.get('Title', 'NA')
+		cover = s_cover_dir + title + s_img_ext
+		author = doc_info.get('Author', 'NA')
+
+		convert_image(pdf_file, cover)
+		print(doc_info)
+		print("https://www.google.co.in/search?q={} by {}".format(title,author))
+		books.append({
+			'author': author,
+			'title': title,
+			'level': 'Intermediate',
+			'title': doc_info.get('Title', 'NA'),
+			'cover': cover,
+			'authorUrl': "https://www.google.co.in/search?q="+author ,
+			'info': 'NA',
+			'url': pdf_file,
+
+		})
+
+with open('issues.json', 'w') as outfile:
+    json.dump({'books':books}, outfile)
